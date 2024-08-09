266c90979e321afa13767b5868d4afccaf55878c
Yash8077 <39164064+Yash8077@users.noreply.github.com>
2024-08-09 23:57:47 +0530

    Update app.js

--- app.js
+++ app.js
@@ -24,22 +24,6 @@ mongoose.connect(mongoURI)
 app.use(bodyParser.urlencoded({extended:true}));
 
 app.use(express.static('./'));
-//Prevent Spin Down
-const url = `https://shree-sahiram-caterering.onrender.com`; // Replace with your Render URL
-const interval = 300000; // Interval in milliseconds (30 seconds)
-
-function reloadWebsite() {
-  axios.get(url)
-    .then(response => {
-      console.log(`Reloaded at ${new Date().toISOString()}: Status Code ${response.status}`);
-    })
-    .catch(error => {
-      console.error(`Error reloading at ${new Date().toISOString()}:`, error.message);
-    });
-}
-
-
-setInterval(reloadWebsite, interval);
 const store= new MongoDBStore({
     uri:mongoURI,
     collection:'userSessions',
